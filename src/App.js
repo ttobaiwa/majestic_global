9be71f70d9636a1135728d4f0c009fcd758f112b
ttobaiwa <117283424+ttobaiwa@users.noreply.github.com>
2022-11-08 13:59:29 +0000

    added new template

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import useContentful from './helpers/useContentful'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Body from './components/Body'
-import Home from './components/sections/Home'
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import useContentful from "./helpers/useContentful";
+import FooterNew from "./components/FooterNew";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
 
-export default function App () {
-	const [siteConfig, setSiteConfig] = useState([])
-	const { getSiteConfig } = useContentful()
-	useEffect(() => {
-		getSiteConfig().then((response) => response && setSiteConfig(response))
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [])
-	return (
-		<>
-			<Header/>
-			<Routes>
-				<Route index element={<Home />}/>
-				<Route path='/:page' element={<Body siteConfigs={siteConfig} />}/>
-				<Route path='*' element={<Body siteConfigs={siteConfig} />} />
-			</Routes>
-			<Footer/>
-		</>
-	)
+export default function App() {
+  const [siteConfig, setSiteConfig] = useState([]);
+  const { getSiteConfig } = useContentful();
+  useEffect(() => {
+    getSiteConfig().then((response) => response && setSiteConfig(response));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route index element={<Main />} />
+        <Route path="/:page" element={<Main siteConfigs={siteConfig} />} />
+        <Route path="*" element={<Main siteConfigs={siteConfig} />} />
+      </Routes>
+      <FooterNew />
+    </>
+  );
 }
